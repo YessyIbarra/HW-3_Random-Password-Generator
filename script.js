938fc3cc9c4f4f41e6f159df57b41ad387801968
Yesenia Ibarra <yessyibarra17@gmail.com>
2020-06-01 23:10:36 -0700

    Cleaned up code.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// This is where we are capturing the element (in HTML file) so we have refrence to it here
+// This is where we are capturing the element (in HTML file) so we have refrence to this JS file
 let generateBtn = document.querySelector("#generate");
 
 // Adding event listener to have the 'writepassword' function run when button is clicked
@@ -18,56 +18,58 @@ const symbolArray = ["~", "{", "|", "}", "`", "_", "^", "[", "]", "@", "?", ">",
 function writePassword() {
   let newPasswordArray = [];
   let passwordCharacters = [];
-  let passwordLength = prompt("Enter number of desired characters for password")
-  let num1 = parseInt (passwordLength);
+  let passwordLength = prompt("Number of Characters")
+  let num1 = parseInt(passwordLength);
   if (num1) { //If user answers this is a string if a user doesnt answer it's null 
-    if (num1 < 8 || num1 > 128) {  
-      alert("Just stop bro");
-    }
-    else{ 
-    if (confirm("Click 'Okay' if would you like to include lowercase letters")) {
 
-      passwordCharacters.push.apply(passwordCharacters, abcLowerArray); //Here I want to merge the two arrays (lower case array and the empty array) 
+    if (num1 < 8 || num1 > 128) {
+      alert("8 < Number of Characters < 128");
+    }
+    else {
+      if (confirm("Include Lowercase Letters?")) {
+
+        passwordCharacters.push.apply(passwordCharacters, abcLowerArray); //Here I want to merge the two arrays (lower case array and the empty array) 
+        console.log(passwordCharacters);
+      }
+
+      if (confirm("Include Uppercase Letters?")) {
+        passwordCharacters.push.apply(passwordCharacters, abcUpperArray);
+        console.log(passwordCharacters);
+      }
+
+      if (confirm("Include Numbers?")) {
+        passwordCharacters.push.apply(passwordCharacters, numberArray);
+        console.log(passwordCharacters);
+      }
+
+      if (confirm("Include Special Characters?")) {
+        passwordCharacters.push.apply(passwordCharacters, symbolArray);
+        console.log(passwordCharacters);
+      }
+      console.log(typeof num1);
       console.log(passwordCharacters);
-    }
 
-    if (confirm("Click 'Okay' if you would like to add UPPERCASE letters")) {
-      passwordCharacters.push.apply(passwordCharacters, abcUpperArray);
-      console.log(passwordCharacters);
-    }
-
-    if (confirm("Click 'Okay' if you would you like to add numbers (#'s)")) {
-      passwordCharacters.push.apply(passwordCharacters, numberArray);
-      console.log(passwordCharacters);
-    }
-
-    if (confirm("Click 'Okay' if you would you like to add special characters ($%*#)")) {
-      passwordCharacters.push.apply(passwordCharacters, symbolArray);
-      console.log(passwordCharacters);
-    }
-    console.log(typeof num1);
-    console.log(passwordCharacters);
-
-    if (passwordCharacters.length===0) {
-      alert("You must select at least 1 type of character for password");
-    }
-    //For Loor
-    for (let i = 0; i <num1; i++) {
-      let genRand = Math.floor(Math.random()*passwordCharacters.length);
-      console.log(genRand);
-      newPasswordArray.push(passwordCharacters[genRand]);
-      console.log(passwordCharacters[genRand])
+      if (passwordCharacters.length === 0) {
+        alert("You Must Select at Least 1 Type of Character");
+      }
+//For Loop
+      for (let i = 0; i < num1; i++) {
+        let genRand = Math.floor(Math.random() * passwordCharacters.length);
+        console.log(genRand);
+        newPasswordArray.push(passwordCharacters[genRand]);
+        console.log(passwordCharacters[genRand])
+      }
     }
   }
-}
+//Printing result to screen
   console.log(newPasswordArray);
   let newPassword = newPasswordArray.join('');
   let passwordText = document.querySelector("#password");
   passwordText.value = newPassword;
-}  
+}
 
 
-  
+
 
 
 

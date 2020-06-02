3c482aa366f66c370298061a84f839ea53a0f5a2
Yesenia Ibarra <yessyibarra17@gmail.com>
2020-06-01 20:16:11 -0700

    Figured out how to correctly merge arrays. Now need to work on the generation of the password.

--- script.js
+++ script.js
@@ -3,78 +3,59 @@ let generateBtn = document.querySelector("#generate");
 
 // Adding event listener to have the 'writepassword' function run when button is clicked
 generateBtn.addEventListener("click", writePassword);
+
 //These are my array variables
-const abcLowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v", "w", "x","y","z"];
-const abcUpperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V", "W", "X","Y","Z"];
-const numberArray = ["0","1","2","3","4","5","6","7","8","9"];
-const symbolArray = ["~","{","|","}","`","_","^","[","]","@","?",">","<","=",";",":","/",".","-",",","+","*","(",")","&","%","$","#"];
+const abcLowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const abcUpperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbolArray = ["~", "{", "|", "}", "`", "_", "^", "[", "]", "@", "?", ">", "<", "=", ";", ":", "/", ".", "-", ",", "+", "*", "(", ")", "&", "%", "$", "#"];
 
 
 // This is where we will begin our password generator function
 //Need to add empty string for the new generated password
 //Add empty array to push all constant variables into
 function writePassword() {
-  let newPassword = "";
+  let newPassword = [];
   let passwordCharacters = [];
   let passwordLength = prompt("Enter number of desired characters for password")
-  if (passwordLength <8 || passwordLength>128) {
-    alert("Password must be greater than 8 characters and less than 128 characters")
-  }
-  else {
+  let num1 = parseInt (passwordLength);
+  if (num1) { //If user answers this is a string if a user doesnt answer it's null 
     if (confirm("Click 'Okay' if would you like to include lowercase letters")) {
-//Here I want to merge the two arrays (lower case array and the empty array) 
-      Array.prototype.push.(passwordCharacters, abcLowerArray);
+      //Here I want to merge the two arrays (lower case array and the empty array) 
+      passwordCharacters.push.apply(passwordCharacters, abcLowerArray);
     }
 
-
     if (confirm("Click 'Okay' if you would like to add UPPERCASE letters")) {
-
+      passwordCharacters.push.apply(passwordCharacters, abcUpperArray);
     }
 
     if (confirm("Click 'Okay' if you would you like to add numbers (#'s)")) {
-
+      passwordCharacters.push.apply(passwordCharacters, numberArray);
     }
 
     if (confirm("Click 'Okay' if you would you like to add special characters ($%*#)")) {
+      passwordCharacters.push.apply(passwordCharacters, symbolArray);
+    }
+    console.log(typeof num1);
 
-
+    for (let i = 0; i <num1; i++) {
+      let genRand = Math.floor(Math.random(passwordCharacters.length));
+      newPassword.push(passwordCharacters[genRand]);
+      console.log(passwordCharacters[genRand])
+      //password characters are added to the empty string
     }
   }
-//this is where we will write the code to be executed -YI
-  let password = generatePassword();
-
-
-
-//this will print the password to screen -YI
-  let passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
+  console.log(newPassword);
 }
 
+  //this is where we need to figure out how to print password to screen after we convert the array of numbers to a string. 
+  //let passwordText = document.querySelector("#password");
+  //passwordText.value = newPassword;
 
 
-//## Acceptance Criteria
 
-//```
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password - done 
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
 
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
 
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, 
-//uppercase, 
-//numeric, 
-//and/or special characters  (I will probably use the || and && here)
 
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
 
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+
